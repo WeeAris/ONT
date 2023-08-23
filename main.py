11e6443d1b6caaf40ec98933d5561d119866102c
WeeAris <madoka33424@gmail.com>
2023-08-23 11:50:53 +0800

    [1/3] Refactor to add txt support

--- main.py
+++ main.py
@@ -1,156 +1,13 @@
 #!/usr/bin/env python3
 import argparse
-import logging
 import os
 import shutil
 import time
 
-import ruamel.std.zipfile as zipfile
-from bs4 import BeautifulSoup
-from ruamel.std.zipfile import delete_from_zip_file
-
-from tools import load_config, extra
+from tools.boo_loader import *
 
 load_config.configure_logging()
 logger = logging.getLogger(__name__)
-
-
-class Boo:
-
-    def __init__(self, book_p):
-        self.book_path = book_p
-
-    target_tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5']
-    noise_tags = []
-
-    def read_epub(self):
-        # 打开epub文件
-        pages = []
-        with zipfile.ZipFile(self.book_path, 'r') as epub_file:
-            file_names = epub_file.namelist()
-            # 读取epub文件中的container.xml文件
-            if 'META-INF/container.xml' not in file_names:
-                print("No 'META-INF/container.xml' file, invalid epub file.")
-                return [], []
-
-            with epub_file.open('META-INF/container.xml', 'r') as container_file:
-                soup = BeautifulSoup(container_file.read(), 'lxml-xml')
-                content_opf_path = \
-                    soup.rootfiles.find('rootfile', attrs={'media-type': "application/oebps-package+xml"})['full-path']
-            with epub_file.open(content_opf_path, 'r') as content_opf:
-                soup = BeautifulSoup(content_opf.read(), 'lxml-xml')
-                items = soup.manifest.find_all('item', attrs={'media-type': "application/xhtml+xml"})
-                items_hrefs = [content_opf_path.replace('content.opf', item['href']) for item in items]
-            for href in items_hrefs:
-                with epub_file.open(href, 'r') as page_file:
-                    pages.append(page_file.read())
-        return items_hrefs, pages
-
-    # 提取需要翻译的文本
-
-    def extract_text_from_pages(self, pages_data: list) -> list[list[str]]:
-        BeautifulSoup(features='lxml')
-        all_pages_text = []
-        for page_data in pages_data:
-            soup = BeautifulSoup(page_data, features='lxml')
-            texts: list[str] = [tag.get_text() for tag in soup.find_all(self.target_tags) if tag.get_text().strip()]
-            all_pages_text.append(texts)
-
-        return all_pages_text
-
-    @staticmethod
-    def extract_titles_from_pages(pages_data: list) -> list[list[str]]:
-        BeautifulSoup(features='lxml')
-        all_titles = []
-        for page_data in pages_data:
-            soup = BeautifulSoup(page_data, features='lxml')
-            titles = []
-            try:
-                t1 = soup.title.get_text()
-                if len(t1.strip()) > 2:
-                    titles.append(t1)
-            except AttributeError:
-                pass
-            for s in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5']):
-                t = s.get_text()
-                if len(t.strip()) > 2 and t not in titles:
-                    titles.append(t)
-            for s in soup.find_all(attrs={"epub:type": "title"}):
-                t = s.get_text()
-                if len(t.strip()) > 2 and t not in titles:
-                    titles.append(t)
-            filter_titles = [title for title in titles if extra.is_text(title)]
-            if filter_titles:
-                all_titles.append(filter_titles)
-
-        return all_titles
-
-    @staticmethod
-    def modify_indent(a: str, b: str) -> str:
-        if b.startswith(' '):
-            num_spaces = len(b) - len(b.lstrip())
-            return (' ' * num_spaces) + a.lstrip()
-        elif b.startswith('\t'):
-            num_spaces = len(b) - len(b.lstrip())
-            return ('\t' * num_spaces) + a.lstrip()
-        else:
-            return a
-
-    # 用译文替换原文，并保留排版
-    def apply_trans_to_pages(self, pages_data: list[bytes], original_contents: list[list[str]],
-                             trans_cts: list[list[str]]) -> list[bytes]:
-        translated_pages = []
-        BeautifulSoup(features='lxml')
-        for idx, page in enumerate(pages_data):
-            soup = BeautifulSoup(page, features='lxml')
-            para_count = 0
-            for node in soup.find_all(self.target_tags):
-                para = node.text
-                if para and para == original_contents[idx][para_count]:
-                    para_trans = trans_cts[idx][para_count]
-                    trans_split = para_trans.splitlines()
-                    trans_split = [sub for sub in trans_split if sub]
-                    # 处理一个标签里有多行内容的问题😅
-                    if len(trans_split) > 1 and len(trans_split) == len(
-                            [sub.get_text().strip() for sub in node.contents if sub.get_text().strip()]):
-                        trans_gen = (sub for sub in trans_split)
-                        for sub in node.contents:
-                            if sub.get_text().strip():
-                                try:
-                                    sub_trans = next(trans_gen)
-                                    sub.string = sub_trans
-                                except StopIteration:
-                                    logger.error("Error while applying translation to a tag with multiple lines.")
-                                    break
-                    else:
-                        para_trans = self.modify_indent(para_trans, para)
-                        node.string = para_trans
-                    para_count += 1
-            # logger.debug(f"页面{idx}共有{para_count}段被成功还原.")
-            translated_pages.append(str(soup).encode('utf-8'))
-        return translated_pages
-
-    @staticmethod
-    def add_title_glossary(orig: list[list[str]], trans: list[list[str]], old_glossary: dict) -> dict:
-        flattened_trans_titles = [item for sublist in trans for item in sublist]
-        flattened_orig_titles = [item for sublist in orig for item in sublist]
-        if len(flattened_orig_titles) == len(flattened_trans_titles):
-            logger.info("Start adding titles to the glossary")
-            for idx, orig_title in enumerate(flattened_orig_titles):
-                if orig_title not in old_glossary.keys():
-                    old_glossary[orig_title] = {
-                        "trans": flattened_trans_titles[idx],
-                        "class": "title"
-                    }
-        return old_glossary
-
-    @staticmethod
-    def write_pages(tg_path: str, pg_hrefs: list[str], translated_pages_data: list):
-        delete_from_zip_file(tg_path, file_names=pg_hrefs)
-        with zipfile.ZipFile(tg_path, 'a') as epub_file:
-            for idx, href in enumerate(pg_hrefs):
-                epub_file.writestr(href, translated_pages_data[idx])
-
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -247,10 +104,10 @@ if __name__ == '__main__':
     if os.path.exists(tmp_path) and os.path.isfile(tmp_path):
         os.remove(tmp_path)
     shutil.copy2(book_path, tmp_path)
-    book = Boo(tmp_path)
+    book = EpubBoo(tmp_path)
 
     # 读取epub文件
-    page_hrefs, all_pages_data = book.read_epub()
+    page_hrefs, all_pages_data = book.read_book()
     orig_titles = book.extract_titles_from_pages(all_pages_data)
     orig_pgs_texts = book.extract_text_from_pages(all_pages_data)
 
